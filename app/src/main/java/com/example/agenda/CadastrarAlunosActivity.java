e180d013162e1c9657439d0b6d1a90b15efda7f7
joao <jvabgo@gmail.com>
2025-10-31 17:28:35 -0300

    feat: criando salvar aluno

--- app/src/main/java/com/example/agenda/CadastrarAlunosActivity.java
+++ app/src/main/java/com/example/agenda/CadastrarAlunosActivity.java
@@ -1,6 +1,10 @@
 package com.example.agenda;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +18,31 @@ public class CadastrarAlunosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_alunos);
+
+        Button botaosalvar = findViewById(R.id.activity_cadastrar_aluno_botao);
+
+        final EditText nomeAluno = findViewById(R.id.activity_cadastrar_aluno_nome);
+        final EditText telefoneAluno = findViewById(R.id.activity_cadastrar_aluno_telefone);
+        final EditText emailAluno = findViewById(R.id.activity_cadastrar_aluno_email);
+
+
+
+        botaosalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nome = nomeAluno.getText().toString();
+                String telefone = telefoneAluno.getText().toString();
+                String email = emailAluno.getText().toString();
+
+                var alunoCriado = new Aluno(nome, telefone, email);
+
+
+                Toast.makeText(CadastrarAlunosActivity.this, "Cadastrado com sucesso!", Toast.LENGTH_LONG).show();
+                Toast.makeText(CadastrarAlunosActivity.this, alunoCriado.getNome(), Toast.LENGTH_LONG).show();
+                Toast.makeText(CadastrarAlunosActivity.this, alunoCriado.getTelefone(), Toast.LENGTH_LONG).show();
+                Toast.makeText(CadastrarAlunosActivity.this, alunoCriado.getEmail(), Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
